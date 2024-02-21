a074d983ac566c6057f845340a9b80a4b038ebf4
Brian Carcich <briantcarcich@gmail.com>
2024-02-20 19:29:39 -0500

    Updated PLC program to add unrolled version [Kernighan's way] algorithm, and plot the results

--- plottimes.py
+++ plottimes.py
@@ -10,11 +10,11 @@ if "__main__" == __name__:
   p = L500D()
   for line in sys.stdin: p.parse_one_line(line)
 
-  time_names = [p.files[k] for k in sorted(p.files.keys()) if p.files[k].endswith('TIME')]
+  time_names = [p.files[k].strip('_') for k in sorted(p.files.keys()) if p.files[k].endswith('TIME') or p.files[k].endswith('UNRL')]
 
   plt.plot([v for v in getattr(p,'BITSPRBYTE')],label='bits/byte',lw=0.5)
   for time_name in time_names:
-    plt.plot([1e-4*v for v in getattr(p,time_name)],label=time_name[:-4])
+    plt.plot([1e-4*v for v in getattr(p,time_name)],label=time_name[:-4] if time_name.endswith('TIME') else time_name)
   plt.xlabel('Unsigned byte value')
   plt.ylabel('Mean processing time per iteration, ms\nOR (# of bits)/16')
   plt.legend(loc='upper left')
